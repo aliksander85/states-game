47084a61d4a5e25acd9aaeef5b2b5959c3a1db5b
Alexander Potapov <potapov.av.85@gmail.com>
2017-02-22 20:09:34 +0300

    animated home page

--- public/js/home.controller.js
+++ public/js/home.controller.js
@@ -1,13 +1,22 @@
 angular
     .module('statesGame')
-    .controller('HomeController', ['$state', function ($state) {
+    .controller('HomeController', ['$state', '$timeout', function ($state, $timeout) {
         var vm = this;
         vm.countries = ['Australia', 'Brazil', 'Canada', 'USA'];
         vm.selected = "";
 
+        var countrySelect = angular.element('#country-select');
+
         vm.startGame = function () {
             if (angular.isString(vm.selected) && vm.selected.length > 0) {
                 $state.go('game', { id: vm.selected.toLowerCase() });
+            } else {
+                countrySelect.focus();
+                countrySelect.addClass('shake');
+
+                $timeout(function () {
+                    countrySelect.removeClass('shake');
+                }, 200);
             }
         };
     }]);
